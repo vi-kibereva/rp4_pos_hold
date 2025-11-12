287b96fe693827851cf741bdc5eab686bd7798d0
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-11-12 18:08:10 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -135,7 +135,7 @@
 
 int main() {
     // Open default camera (0 = /dev/video0)
-    cv::VideoCapture cap(0);
+    cv::VideoCapture cap(0, cv::CAP_LIBCAMERA);
 
     if (!cap.isOpened()) {
         std::cerr << "Error: Could not open camera" << std::endl;
