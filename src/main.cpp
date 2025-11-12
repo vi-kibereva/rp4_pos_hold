ab77c738fd73b77bfa0e4b264878af8c98f18ae8
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-11-12 18:07:39 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -143,8 +143,8 @@ int main() {
     }
 
     // Optional: set resolution and FPS
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
     // cap.set(cv::CAP_PROP_FPS, 30);
 
     // Get the frame size
