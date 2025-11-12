9b60a52a1d3800781a6f026722f0428b9b0bc2af
yagodanr <yagodanr@gmail.com>
2025-11-12 18:32:44 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -132,28 +132,69 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+
+
+
+#include <new>
+#include <string>
+#include <sstream>
+
+#define INPUT_WIDTH 3264
+#define INPUT_HEIGHT 2464
+
+#define DISPLAY_WIDTH 640
+#define DISPLAY_HEIGHT 480
+
+#define CAMERA_FRAMERATE 21/1
+#define FLIP 2
+
+
 #include <raspicam/raspicam_cv.h>
 
-int main()
+
+void DisplayVersion()
 {
-    using namespace std;
-    using namespace cv;
+    std::cout << "OpenCV version: " << cv::getVersionMajor() << "." << cv::getVersionMinor() << "." << cv::getVersionRevision() << std::endl;
+}
 
-    // Create camera object
-    raspicam::RaspiCam_Cv cap;
 
-    // Set camera properties if needed
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    cap.set(cv::CAP_PROP_FPS, 30);
+int main(int argc, const char** argv)
+{
 
-    // Open camera
-    if (!cap.open()) {
-        cerr << "Could not initialize capturing..." << endl;
+    DisplayVersion();
+
+    std::stringstream ss;
+
+    ss << "nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=2 ! video/x-raw, width=480, height=680, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+
+    //ss << "nvarguscamerasrc !  video/x-raw(memory:NVMM), width=" << INPUT_WIDTH <<
+    //", height=" << INPUT_HEIGHT <<
+    //", format=NV12, framerate=" << CAMERA_FRAMERATE <<
+    //" ! nvvidconv flip-method=" << FLIP <<
+    //" ! video/x-raw, width=" << DISPLAY_WIDTH <<
+    //", height=" << DISPLAY_HEIGHT <<
+    //", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+
+    cv::VideoCapture video;
+
+    video.open(ss.str());
+
+    if (!video.isOpened())
+    {
+        std::cout << "Unable to get video from the camera!" << std::endl;
+
         return -1;
     }
 
-    cout << "Camera opened successfully!" << endl;
+    std::cout << "Got here!" << std::endl;
+
+    cv::Mat frame;
 
     // Create VideoWriter
     int frame_width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
@@ -175,9 +216,9 @@ int main()
     for (int i = 0; i < 150; ++i) {
 		std::cout << i << '\n';
         // Grab and retrieve frame
-        cap.grab();
-        cap.retrieve(frame);
-
+        // cap.grab();
+        // cap.retrieve(frame);
+        video.read(frame);
         if (frame.empty()) {
             cerr << "Empty frame, exiting..." << endl;
             break;
@@ -186,11 +227,86 @@ int main()
         // Write frame to video
         writer.write(frame);
     }
-
-    cap.release();
+    // video.release()
     writer.release();
     destroyAllWindows();
 
     cout << "Video saved as output.mp4" << endl;
     return 0;
+
+//     while (video.read(frame))
+//     {
+//         cv::imshow("Video feed", frame);
+
+//         if (cv::waitKey(25) >= 0)
+//         {
+//             break;
+//         }
+//    }
+
+//     std::cout << "Finished!" << std::endl;
+
+//     return 0;
 }
+
+
+// int main()
+// {
+//     using namespace std;
+//     using namespace cv;
+
+//     // Create camera object
+//     raspicam::RaspiCam_Cv cap;
+
+//     // Set camera properties if needed
+//     cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+//     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+//     cap.set(cv::CAP_PROP_FPS, 30);
+
+//     // Open camera
+//     if (!cap.open()) {
+//         cerr << "Could not initialize capturing..." << endl;
+//         return -1;
+//     }
+
+//     cout << "Camera opened successfully!" << endl;
+
+//     // Create VideoWriter
+//     int frame_width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
+//     int frame_height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
+//     VideoWriter writer(
+//         "output.mp4",
+//         VideoWriter::fourcc('m','p','4','v'),  // MP4
+//         30.0,
+//         Size(frame_width, frame_height)
+//     );
+//     if (!writer.isOpened()) {
+//         cerr << "Could not open output file for write" << endl;
+//         return -1;
+//     }
+
+//     cout << "Recording... Press ESC to stop." << endl;
+
+//     Mat frame;
+//     for (int i = 0; i < 150; ++i) {
+// 		std::cout << i << '\n';
+//         // Grab and retrieve frame
+//         cap.grab();
+//         cap.retrieve(frame);
+
+//         if (frame.empty()) {
+//             cerr << "Empty frame, exiting..." << endl;
+//             break;
+//         }
+
+//         // Write frame to video
+//         writer.write(frame);
+//     }
+
+//     cap.release();
+//     writer.release();
+//     destroyAllWindows();
+
+//     cout << "Video saved as output.mp4" << endl;
+//     return 0;
+// }
