93341278577588f7ca79be4a38f40843da761677
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-11-12 17:15:14 +0200

    update

--- include/posHold/VecMove.h
+++ include/posHold/VecMove.h
@@ -16,6 +16,7 @@ public:
 
 private:
     static constexpr int s_accountFlowPixels = 10;
+    static constexpr int s_calcFlowPixels = 20;
     static constexpr double s_noFlowBalanceVecMultiplier = 1.0f;
     Drone* m_drone;
     VecDown m_vecDown;
