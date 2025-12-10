10c0c8aa3fd7088879f9b049b94a8ef828872fde
weqpro <weqpro@gmail.com>
2025-12-10 12:42:49 +0200

    change pid

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]) {
     constexpr uint16_t YAW_VALUE = 1500;       // Neutral yaw
 
     // Default PID gains
-    float k_p = 50.0f;
+    float k_p = 100.0f;
     float k_i = 1.0f;
     float k_d = 1.0f;
     float k_df = 0.0f;
